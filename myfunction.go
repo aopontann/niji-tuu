b03bfc9f9aeec205130189468aaa9ab8ef509ada
aopontann <aopontan0416@gmail.com>
2024-11-23 11:27:19 +0000

    db.goのファクトリ関数の修正に伴う修正&リクエストボディ処理追加 #107

--- myfunction.go
+++ myfunction.go
@@ -7,10 +7,6 @@ import (
 	"os"
 
 	"github.com/GoogleCloudPlatform/functions-framework-go/functions"
-	"github.com/jackc/pgx/v5"
-	"github.com/jackc/pgx/v5/stdlib"
-	"github.com/uptrace/bun"
-	"github.com/uptrace/bun/dialect/pgdialect"
 )
 
 func init() {
@@ -33,20 +29,7 @@ func init() {
 		logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
 		slog.SetDefault(logger) // 以降、JSON形式で出力される。
 
-		config, err := pgx.ParseConfig(os.Getenv("DSN"))
-		if err != nil {
-			panic(err)
-		}
-		sqldb := stdlib.OpenDB(*config)
-		db := bun.NewDB(sqldb, pgdialect.New())
-		defer db.Close()
-
-		job := NewJobs(
-			os.Getenv("YOUTUBE_API_KEY"),
-			db,
-		)
-
-		err = job.CheckNewVideoJob()
+		err := CheckNewVideoJob()
 		if err != nil {
 			slog.Error("CheckNewVideoJob",
 				slog.String("severity", "ERROR"),
@@ -60,20 +43,17 @@ func init() {
 		logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
 		slog.SetDefault(logger) // 以降、JSON形式で出力される。
 
-		config, err := pgx.ParseConfig(os.Getenv("DSN"))
-		if err != nil {
-			panic(err)
+		var b SongTaskReqBody
+		if err := json.NewDecoder(r.Body).Decode(&b); err != nil {
+			slog.Error("NewDecoder error",
+				slog.String("severity", "ERROR"),
+				slog.String("message", err.Error()),
+			)
+			http.Error(w, "リクエストボディが不正です", http.StatusInternalServerError)
+			return
 		}
-		sqldb := stdlib.OpenDB(*config)
-		db := bun.NewDB(sqldb, pgdialect.New())
-		defer db.Close()
 
-		job := NewJobs(
-			os.Getenv("YOUTUBE_API_KEY"),
-			db,
-		)
-
-		err = job.SongVideoAnnounceJob()
+		err := SongVideoAnnounceJob(b.ID)
 		if err != nil {
 			slog.Error("SongVideoAnnounceJob",
 				slog.String("severity", "ERROR"),
@@ -87,22 +67,19 @@ func init() {
 		logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
 		slog.SetDefault(logger) // 以降、JSON形式で出力される。
 
-		config, err := pgx.ParseConfig(os.Getenv("DSN"))
-		if err != nil {
-			panic(err)
+		var b TopicTaskReqBody
+		if err := json.NewDecoder(r.Body).Decode(&b); err != nil {
+			slog.Error("NewDecoder error",
+				slog.String("severity", "ERROR"),
+				slog.String("message", err.Error()),
+			)
+			http.Error(w, "リクエストボディが不正です", http.StatusInternalServerError)
+			return
 		}
-		sqldb := stdlib.OpenDB(*config)
-		db := bun.NewDB(sqldb, pgdialect.New())
-		defer db.Close()
 
-		job := NewJobs(
-			os.Getenv("YOUTUBE_API_KEY"),
-			db,
-		)
-
-		err = job.KeywordAnnounceJob()
+		err := TopicAnnounceJob(b.VID, b.TID)
 		if err != nil {
-			slog.Error("KeywordAnnounceJob",
+			slog.Error("TopicAnnounceJob",
 				slog.String("severity", "ERROR"),
 				slog.String("message", err.Error()),
 			)
