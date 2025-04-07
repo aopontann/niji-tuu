000568dc9abd4437311c3a7a95d15c6fc9d86394
aopontann <aopontan0416@gmail.com>
2025-04-07 23:07:42 +0900

    topic関連のテーブルと処理を削除 #153

--- fcm_test.go
+++ fcm_test.go
@@ -22,34 +22,3 @@ func TestSend(t *testing.T) {
 		t.Error(err)
 	}
 }
-
-func TestSetTopic(t *testing.T) {
-	fcm := NewFCM()
-
-	err := fcm.SetTopic(token, "歌枠")
-	if err != nil {
-		t.Error(err)
-	}
-}
-
-func TestDeleteTopic(t *testing.T) {
-	fcm := NewFCM()
-
-	err := fcm.DeleteTopic(token, "歌枠")
-	if err != nil {
-		t.Error(err)
-	}
-}
-
-func TestSendTopic(t *testing.T) {
-	fcm := NewFCM()
-
-	err := fcm.TopicNotification("歌枠", &NotificationVideo{
-		ID:        "Sqpmvv8uulM",
-		Title:     "心予報/歌わせていただきました。",
-		Thumbnail: "https://i.ytimg.com/vi/OPzbUoLxYyE/default.jpg",
-	})
-	if err != nil {
-		t.Error(err)
-	}
-}
