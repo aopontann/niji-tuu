604fad8828ddfa914245c1e444554784f689dd70
aopontann <aopontan0416@gmail.com>
2025-04-17 23:40:50 +0900

    ハンドラ関数を呼び出すだけに修正 #155

--- myfunction.go
+++ myfunction.go
@@ -2,11 +2,15 @@ package nsa
 
 import (
 	"log/slog"
-	"net/http"
 	"os"
-	"strings"
 
 	"github.com/GoogleCloudPlatform/functions-framework-go/functions"
+	discordbot "github.com/aopontann/niji-tuu/internal/discord/bot"
+	discordnotice "github.com/aopontann/niji-tuu/internal/discord/notice"
+	discordtask "github.com/aopontann/niji-tuu/internal/discord/task"
+	newvideo "github.com/aopontann/niji-tuu/internal/new-video"
+	songnotice "github.com/aopontann/niji-tuu/internal/song/notice"
+	songtask "github.com/aopontann/niji-tuu/internal/song/task"
 )
 
 func init() {
@@ -29,91 +33,15 @@ func init() {
 	logger := slog.New(slog.NewJSONHandler(os.Stdout, &ops))
 	slog.SetDefault(logger)
 
-	functions.HTTP("check", func(w http.ResponseWriter, r *http.Request) {
-		err := CheckNewVideoJob()
-		if err != nil {
-			slog.Error(err.Error())
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-		}
-	})
+	functions.HTTP("new-video", newvideo.Handler)
 
-	functions.HTTP("song-task", func(w http.ResponseWriter, r *http.Request) {
-		vids := r.FormValue("v")
-		if vids == "" {
-			msg := "クエリパラメータ v が指定されていません"
-			slog.Error(msg)
-			http.Error(w, msg, http.StatusBadRequest)
-			return
-		}
+	functions.HTTP("song-task", songtask.Handler)
 
-		err := SongVideoCheck(strings.Split(vids, ","))
-		if err != nil {
-			slog.Error(err.Error())
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-		}
-	})
+	functions.HTTP("discord-task", discordtask.Handler)
 
-	functions.HTTP("discord-task", func(w http.ResponseWriter, r *http.Request) {
-		vids := r.FormValue("v")
-		if vids == "" {
-			msg := "クエリパラメータ v が指定されていません"
-			slog.Error(msg)
-			http.Error(w, msg, http.StatusBadRequest)
-			return
-		}
+	functions.HTTP("song-notice", songnotice.Handler)
 
-		err := CreateTaskToNoficationByDiscord(strings.Split(vids, ","))
-		if err != nil {
-			slog.Error(err.Error())
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-		}
-	})
+	functions.HTTP("discord-notice", discordnotice.Handler)
 
-	functions.HTTP("notice-song", func(w http.ResponseWriter, r *http.Request) {
-		if r.Method != http.MethodPost {
-			msg := "POSTメソッドでリクエストしてください"
-			slog.Error(msg)
-			http.Error(w, msg, http.StatusBadRequest)
-			return
-		}
-
-		vid := r.FormValue("v")
-		if vid == "" {
-			msg := "クエリパラメータ v が指定されていません"
-			slog.Error(msg)
-			http.Error(w, msg, http.StatusBadRequest)
-			return
-		}
-
-		err := SongVideoAnnounceJob(vid)
-		if err != nil {
-			slog.Error(err.Error())
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-		}
-	})
-
-	functions.HTTP("notice-discord", func(w http.ResponseWriter, r *http.Request) {
-		if r.Method != http.MethodPost {
-			msg := "POSTメソッドでリクエストしてください"
-			slog.Error(msg)
-			http.Error(w, msg, http.StatusBadRequest)
-			return
-		}
-		
-		vid := r.FormValue("v")
-		if vid == "" {
-			msg := "クエリパラメータ v が指定されていません"
-			slog.Error(msg)
-			http.Error(w, msg, http.StatusBadRequest)
-			return
-		}
-
-		err := DiscordAnnounceJob(vid)
-		if err != nil {
-			slog.Error(err.Error())
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-		}
-	})
-
-	functions.HTTP("discord-bot", DiscordWebhook)
+	functions.HTTP("discord-bot", discordbot.Handler)
 }
