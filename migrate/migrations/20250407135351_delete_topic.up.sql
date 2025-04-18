65e0648b475fa3b981551ffde0da487c600bb837
aopontann <aopontan0416@gmail.com>
2025-04-19 01:06:41 +0900

    SEQUENCE を削除するSQLを追加

--- migrate/migrations/20250407135351_delete_topic.up.sql
+++ migrate/migrations/20250407135351_delete_topic.up.sql
@@ -1,7 +1,13 @@
 SET statement_timeout = 0;
 
 --bun:split
+DROP SEQUENCE IF NOT EXISTS topics_id_seq;
+
+--bun:split
 DROP TABLE "user_topics";
+
+--bun:split
+DROP SEQUENCE IF NOT EXISTS user_topics_topic_id_seq;
 
 --bun:split
 DROP TABLE "topics";
